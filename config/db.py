6deb7149585865fd98945c2a0f9979ae346db0cf
FacuV <facuun.vicente@gmail.com>
2022-07-25 17:54:02 -0300

    First commit

---
+++ config/db.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine, MetaData
+
+create_engine("mysql+pymysql://root:12345@localhost:3306/")
---
+++ routes/user.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+user = APIRouter()
+
+@user.get("/users")
+def get_users():
+    return {"users": "users"}
+
+@user.get("/users")
+def get_users():
+    return {"users": "users"}
+
+@user.get("/users")
+def get_users():
+    return {"users": "users"}
+
+@user.get("/users")
+def get_users():
+    return {"users": "users"}
+
+@user.get("/users")
+def get_users():
+    return {"users": "users"}
